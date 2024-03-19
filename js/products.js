56c8cbb631d9ad3cbe26158869e7446a151763bc
Libish <libish@portabl.co>
2024-03-19 13:21:59 -0400

    json created for a list of product and load products dynamically from json

---
+++ js/products.js
@@ -0,0 +1,44 @@
+import { clockList } from "./productsList.js";
+
+const prod = document.querySelector(".products");
+// Function to render a single product
+function renderProduct(product) {
+    const htmlData = `
+          <div class="card" data-product-id="${product.id}">
+           
+            <img src=".${product.location}" alt="image1" class="responsiveImage">
+          
+            <div class="clock-details">
+                <h5><b>${product.code}</b></h4> 
+                <h4><b> ${product.title}</b></h4> 
+                <h5> CAD ${product.price}</h5> 
+            </div>
+          </div>`;
+    return htmlData;
+  }
+  
+  // Function to render all products
+  function renderAllProducts() {
+    prod.innerHTML = "";
+    clockList.forEach((product) => {
+      prod.insertAdjacentHTML("beforeend", renderProduct(product));
+    });
+    attachProductClickListeners();
+  }
+
+  // Function to attach click event to each product
+function attachProductClickListeners() {
+    const items = document.querySelectorAll(".card");
+    items.forEach((item) => {
+      item.addEventListener("click", openProductPage);
+    });
+}
+// Function to handle product click
+function openProductPage(event) {
+    debugger;
+    const productId = event.currentTarget.dataset.productId;
+    alert(productId);
+    // window.location.href = `./singleProduct.html?id=${productId}`;
+}
+
+renderAllProducts();
